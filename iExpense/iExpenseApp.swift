69d53fe8562241853be701b804c76ff15ef512e2
Arshya GHAVAMI <arshyaghavami@Arshyas-MacBook-Air.local>
2021-01-19 10:47:34 -0800

    First Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Arshya GHAVAMI on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
